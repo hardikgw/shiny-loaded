61b1961fdb88677f90d2f151759f9a748dc1f895
Hardik Patel <hardikgw@gmail.com>
2016-11-26 11:27:03 -0500

    start script

--- start-shiny.sh
+++ start-shiny.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run -d --name shiny -v $(pwd)/r-data:/srv/shiny-server -v $(pwd):/var/log/shiny-server -p 80:3838 channelit/shiny-loaded
+docker run -d --name shiny -v $(pwd)/r-data:/srv/shiny-server -v $(pwd)/r-logs:/var/log/shiny-server -p 80:3838 channelit/shiny-loaded
